fa0f9897b204b60e73cde30bebc9df8ad02e692b
shubham sharma <shubhamtech004@gmail.com>
2025-06-17 00:05:42 +0530

    navbar section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="bg-blue-900 p-3">
+      <div className="flex justify-around items-center">
+        <div className="flex items-center space-x-2 text-white font-bold text-xl">
+          <NavLink to="/"> <img src="/logo.ico" alt="Web logo" className="w-8 h-8 rounded " />
+          </NavLink><span>Skynow</span>
+        
+        </div>
+
+        {/* Mobile menu button */}
+        <button
+          className="text-white md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} size="lg" />
+        </button>
+
+        {/* Desktop menu */}
+        <div className="hidden md:flex space-x-6 text-white">
+         <NavLink to="/about">About</NavLink>
+         
+        </div>
+      </div>
+
+      {/* Mobile dropdown */}
+      {isOpen && (
+        <div className="flex flex-col mt-2 text-center space-y-2 text-white md:hidden">
+          <NavLink to="/about" onClick={() => setIsOpen(false)}>About</NavLink>
+         
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
